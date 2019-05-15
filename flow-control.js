614da128a0eb7f4682cbec8c2b54bbb6009d67a0
convivial-expression-6540 <jamaltbutler@gmail.com>
2019-05-15 13:20:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -9,7 +9,12 @@ function basicTeenager(age) {
 basicTeenager(15)
 
 function teenager(age) {
-  
+  if (age >=13 && age <=19) {
+   return "You are a teenager!"
+ }else {
+   return ""
+ }
+ }
 }
 
 teenager(17)
